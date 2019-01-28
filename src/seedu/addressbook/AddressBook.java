efe38be4fc55169f8622635df0a5366db59bf61b
JonathanLeeWH <leeweihao1996@outlook.com>
2019-01-28 09:53:30 +0800

    Reformat code
    Fix a small typographical error.
    Reformat code according to coding standard.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -44,7 +44,7 @@ public class AddressBook {
     /**
      * Version info of the program.
      */
-    private static final String VERSION = "AddessBook Level 1 - Version 1.0";
+    private static final String VERSION = "AddressBook Level 1 - Version 1.0";
 
     /**
      * A decorative prefix added to the beginning of lines printed by AddressBook
@@ -218,6 +218,9 @@ public class AddressBook {
         return;
     }
 
+    /**
+     * Parses command line arguments input by the user.
+     */
     private static void parseInputCommand() {
         while (true) {
             String userCommand = getUserInput();
